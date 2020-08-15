5117eb5ef80f4fd19b3cee4e8c6940bdacdcc808
reisub0 <reisub0@gmail.com>
2020-08-16 00:21:49 +0530

    Moves folders into new directory structure
    Signed-off-by: reisub0 <reisub0@gmail.com>

--- breakTime/utils.py
+++ breakTime/utils.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import ctypes
 import tkinter as tk
 from datetime import datetime, timedelta
@@ -14,11 +13,13 @@ class FullScreenApp(object):
     def __init__(self, master, **kwargs):
         self.master = master
         pad = 3
-        self._geom = '200x200+0+0'
-        master.geometry("{0}x{1}+0+0".format(master.winfo_screenwidth() - pad,
-                                             master.winfo_screenheight() -
-                                             pad))
-        master.bind('<Escape>', self.toggle_geom)
+        self._geom = "200x200+0+0"
+        master.geometry(
+            "{0}x{1}+0+0".format(
+                master.winfo_screenwidth() - pad, master.winfo_screenheight() - pad
+            )
+        )
+        master.bind("<Escape>", self.toggle_geom)
 
     def toggle_geom(self, event):
         geom = self.master.winfo_geometry()
@@ -29,10 +30,11 @@ class FullScreenApp(object):
 
 def blockScreen(seconds):
     root = tk.Tk()
-    root.attributes('-fullscreen', True)
-    root.configure(background='black')
-    root.after(int(seconds * 1000),
-               lambda: root.destroy())  # Destroy the widget after 30 seconds
+    root.attributes("-fullscreen", True)
+    root.configure(background="black")
+    root.after(
+        int(seconds * 1000), lambda: root.destroy()
+    )  # Destroy the widget after 30 seconds
     app = FullScreenApp(root)
     root.wm_attributes("-topmost", 1)
     root.mainloop()
@@ -47,12 +49,12 @@ def isAdmin():
 
 # Prevents any input for x seconds, uses subprocess.Popen
 def blockInput(seconds):
-    Popen(f'blockInput.exe {seconds * 1000}')
+    Popen(f"blockInput.exe {seconds * 1000}")
 
 
 def waitForUserActivity():
     firstInputInfo = win32api.GetLastInputInfo()
-    print('Waiting for user activity...')
+    print("Waiting for user activity...")
     while firstInputInfo - win32api.GetLastInputInfo() == 0:
         pass
 
@@ -60,20 +62,17 @@ def waitForUserActivity():
 def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
-        timeformat = '{:02.0f}:{:02.0f}'.format(mins, secs)
+        timeformat = "{:02.0f}:{:02.0f}".format(mins, secs)
         print(f"Next break in {timeformat}", end="\r")
         sleep(1)
         t -= 1
         if t == 15:
-            sendNotification('Break in 15 seconds')
+            sendNotification("Break in 15 seconds")
             pass
         pass
 
 
 def sendNotification(message):
     notification.notify(
-        title='Take A Break',
-        message=message,
-        app_icon='logo.ico',
-        timeout=10,
+        title="Take A Break", message=message, app_icon="logo.ico", timeout=10,
     )
