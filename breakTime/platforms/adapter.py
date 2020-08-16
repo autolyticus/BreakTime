bd577cd7d69d78a97bb193152dd64a721165af83
reisub0 <reisub0@gmail.com>
2020-08-16 06:21:02 +0530

    Code refactor for platforms

--- breakTime/platforms/adapter.py
+++ breakTime/platforms/adapter.py
@@ -1,26 +1,36 @@
-
 import platform
 import re
 
 from typing import Any
+from enum import Enum, auto
+from .IAdapter import IAdapter
+
+
+class Platforms(Enum):
+    Windows = auto
+    Linux = auto
+    Unknown = auto
+
 
 onWindows = re.search("win", platform.system(), re.IGNORECASE) is not None
 
+if onWindows:
+    Platform = Platforms.Windows
 # Right now we only support Windows and Linux
-onLinux = not onWindows
-
-from .IAdapter import IAdapter
+else:
+    Platform = Platforms.Linux
 
 
 class Adapter:
     """ Encapsulates all platform specific code using the Adapter pattern """
+
     @staticmethod
     def get(*args: Any, **kwargs: Any) -> IAdapter:
-        if onWindows:
+        if Platform == Platforms.Windows:
             from .windows import WindowsAdapter
 
             return WindowsAdapter(*args, **kwargs)
-        elif onLinux:
+        elif Platform == Platforms.Linux:
             from .linux import LinuxAdapter
 
             return LinuxAdapter(*args, **kwargs)
--- breakTime/platforms/windows/__init__.py
+++ breakTime/platforms/windows/__init__.py
@@ -1,5 +1,19 @@
+import ctypes
+import sys
+
 from ..IAdapter import IAdapter
 
 
 class WindowsAdapter(IAdapter):
-    pass
+    @staticmethod
+    def relaunchAsAdmin():
+        ctypes.windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, " ".join(sys.argv), None, 1
+        )
+
+    @staticmethod
+    def isAdmin():
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
