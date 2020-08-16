30891d4ddd9ec8b78d099de379a726dbbbfebeec
reisub0 <reisub0@gmail.com>
2020-08-16 06:40:09 +0530

    Adds `schedule` based code

--- breakTime/__main__.py
+++ breakTime/__main__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
+import schedule
+import threading
+from time import sleep
+
 from .cli import parseArgs
 from .platforms.adapter import Adapter, Platforms, Platform
 
@@ -12,3 +16,20 @@ adapter = Adapter.get()
 if Platform == Platforms.Windows:
     if not adapter.isAdmin():
         adapter.relaunchAsAdmin()
+
+
+def mainLoop():
+    pass
+
+
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
+
+
+schedule.every(5).seconds.do(run_threaded, mainLoop)
+
+
+while True:
+    schedule.run_pending()
+    sleep(5)
