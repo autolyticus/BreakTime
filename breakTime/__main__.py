39043449052314d70adf4c4917f6d7bfb604338c
reisub0 <reisub0@gmail.com>
2020-08-16 15:10:41 +0530

    Adds logging support

--- breakTime/__main__.py
+++ breakTime/__main__.py
@@ -3,10 +3,21 @@
 import sys
 import schedule
 import threading
+import logging
+
 from time import sleep
 
 from .cli import parseArgs
 from .platforms.adapter import Adapter, Platforms, Platform
+from .Timer import Timer
+
+DEBUG = 1
+
+if DEBUG:
+    logging.basicConfig(
+        format="%(asctime)s %(filename)s:%(lineno)d %(levelname)s:%(message)s",
+        level=logging.INFO,
+    )
 
 
 workMins, breakMins = parseArgs(sys.argv)
@@ -21,7 +32,7 @@ t = Timer(workMins, breakMins)
 
 
 def mainLoop():
-    t.update()
+    t.tick()
 
 
 schedule.every(5).seconds.do(mainLoop)
